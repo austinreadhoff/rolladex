7acdd699b202cf0ab6ad35390d5f4a614244ca09
austinread <aread1800@gmail.com>
2021-12-12 17:48:28 -0500

    Init save warnings for all spell inputs

--- ts/renderer/json-io-renderer.ts
+++ ts/renderer/json-io-renderer.ts
@@ -26,7 +26,7 @@ ipcRenderer.on('send-loaded-json', (event: any, json: any) => {
     document.querySelectorAll('input').forEach(input => {
         if (input.classList.contains("attack-stat") 
         || input.classList.contains("misc-counter")
-        || input.classList.contains("spell-name")){
+        || input.classList.contains("spell-input")){
             input.addEventListener('input', event => {
                 triggerUnsafeSave();
             });
