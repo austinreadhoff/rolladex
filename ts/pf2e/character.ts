e89ca544f49bdfd7677898fdcf5c2752901c01aa
austinread <aread1800@gmail.com>
2022-09-05 11:42:49 -0400

    wip ability scores

--- ts/pf2e/character.ts
+++ ts/pf2e/character.ts
@@ -18,6 +18,29 @@ export class Character {
     diety: KnockoutObservable<string>;
     level: KnockoutObservable<string>;
     heroPoints: KnockoutObservable<string>;
+    str: KnockoutObservable<string>;
+    dex: KnockoutObservable<string>;
+    con: KnockoutObservable<string>;
+    int: KnockoutObservable<string>;
+    wis: KnockoutObservable<string>;
+    char: KnockoutObservable<string>;
+    perception: KnockoutObservable<string>;
+    acrobatics: KnockoutObservable<string>;
+    arcana: KnockoutObservable<string>;
+    athletics: KnockoutObservable<string>;
+    crafting: KnockoutObservable<string>;
+    deception: KnockoutObservable<string>;
+    diplomacy: KnockoutObservable<string>;
+    intimidation: KnockoutObservable<string>;
+    medicine: KnockoutObservable<string>;
+    nature: KnockoutObservable<string>;
+    occultism: KnockoutObservable<string>;
+    performance: KnockoutObservable<string>;
+    religion: KnockoutObservable<string>;
+    society: KnockoutObservable<string>;
+    stealth: KnockoutObservable<string>;
+    survival: KnockoutObservable<string>;
+    thievery: KnockoutObservable<string>;
 
     constructor(){
         this.version = ko.observable(jsonSchemaVersion);
@@ -34,9 +57,35 @@ export class Character {
         this.diety = ko.observable("");
         this.level = ko.observable("");
         this.heroPoints = ko.observable("");
+        this.str = ko.observable("");
+        this.dex = ko.observable("");
+        this.con = ko.observable("");
+        this.int = ko.observable("");
+        this.wis = ko.observable("");
+        this.char = ko.observable("");
+        this.perception = ko.observable("U");
+        this.acrobatics = ko.observable("U");
+        this.arcana = ko.observable("U");
+        this.athletics = ko.observable("U");
+        this.crafting = ko.observable("U");
+        this.deception = ko.observable("U");
+        this.diplomacy = ko.observable("U");
+        this.intimidation = ko.observable("U");
+        this.medicine = ko.observable("U");
+        this.nature = ko.observable("U");
+        this.occultism = ko.observable("U");
+        this.performance = ko.observable("U");
+        this.religion = ko.observable("U");
+        this.society = ko.observable("U");
+        this.stealth = ko.observable("U");
+        this.survival = ko.observable("U");
+        this.thievery = ko.observable("U");
+
 
         let savedProperties: string[] = ['characterName', 'playerName', 'xp', 'ancestryHeritage', 'background', 'characterClass',
-         'size', 'alignment', 'traits', 'diety', 'level', 'heroPoints'];
+         'size', 'alignment', 'traits', 'diety', 'level', 'heroPoints', 'str', 'dex', 'con', 'int', 'wis', 'char',
+         'perception', 'acrobatics', 'arcana', 'athletics', 'crafting', 'deception', 'diplomacy', 'intimidation', 'medicine',
+         'nature', 'occultism', 'performance', 'religion', 'society', 'stealth', 'survival', 'thievery'];
         for(var p in savedProperties)
         {
             let propStr = savedProperties[p];
@@ -46,5 +95,39 @@ export class Character {
                 (this[propName] as any).subscribe(function(){triggerUnsafeSave(viewModel.character().characterName());});
             }
         }
+    }
+
+    abilityMod(ability: number, proficiencyLevel = "U"){
+        return ko.computed(() =>{
+            if (!ability) return "+0";
+
+            let proficiencyBonus: number;
+            switch(proficiencyLevel) { 
+                case "T": { 
+                    proficiencyBonus = 2 + +this.level();
+                    break; 
+                } 
+                case "E": { 
+                    proficiencyBonus = 4 + +this.level();
+                    break; 
+                } 
+                case "M": { 
+                    proficiencyBonus = 6 + +this.level();
+                    break; 
+                } 
+                case "L": { 
+                    proficiencyBonus = 8 + +this.level();
+                    break; 
+                } 
+                default: { 
+                    proficiencyBonus = 0; 
+                    break; 
+                } 
+            } 
+
+            let mod = Math.floor(+ability / 2) - 5 + (proficiencyBonus)
+            if (!mod) return "+0";
+            return mod < 0 ? mod.toString() : ("+" + mod);
+        }, this);
     }
 }
--- ts/pf2e/viewmodel.ts
+++ ts/pf2e/viewmodel.ts
@@ -13,5 +13,74 @@ export var viewModel = new ViewModel(new Character);
 
 //to be executed on document ready
 export function applyDataBinding(){
+    //valueAccessor: proficiency string
+    ko.bindingHandlers.skillbox = {
+        init: function(element: HTMLElement, valueAccessor: any){
+            let profString = valueAccessor();
+            let value = ko.unwrap(profString());
+
+            element.addEventListener("click", event => {
+                if (element.classList.contains("skillbox-untrained")){
+                    element.classList.remove("skillbox-untrained");
+                    element.classList.add("skillbox-trained");
+                    element.innerHTML = "T";
+                    profString("T");
+                }
+                else if (element.classList.contains("skillbox-trained")){
+                    element.classList.remove("skillbox-trained");
+                    element.classList.add("skillbox-expert");
+                    element.innerHTML = "E";
+                    profString("E");
+                }
+                else if (element.classList.contains("skillbox-expert")){
+                    element.classList.remove("skillbox-expert");
+                    element.classList.add("skillbox-master");
+                    element.innerHTML = "M";
+                    profString("M");
+                }
+                else if (element.classList.contains("skillbox-master")){
+                    element.classList.remove("skillbox-master");
+                    element.classList.add("skillbox-legendary");
+                    element.innerHTML = "L";
+                    profString("L");
+                }
+                else if (element.classList.contains("skillbox-legendary")){
+                    element.classList.remove("skillbox-legendary");
+                    element.classList.add("skillbox-untrained");
+                    element.innerHTML = "U";
+                    profString("U");
+                }
+            }, true);
+        },
+        update(element: HTMLElement, valueAccessor: any){
+            let profString = valueAccessor();
+            let value = ko.unwrap(profString());
+
+            switch(value){
+                case "T":
+                    element.innerHTML = "T";
+                    element.className = "skillbox skillbox-trained";
+                    break;
+                case "E":
+                    element.innerHTML = "E";
+                    element.className = "skillbox skillbox-expert";
+                    break;
+                case "M":
+                    element.innerHTML = "M";
+                    element.className = "skillbox skillbox-master";
+                    break;
+                case "L":
+                    element.innerHTML = "L";
+                    element.className = "skillbox skillbox-legendary";
+                    break;
+                case "U":
+                default:
+                    element.innerHTML = "U";
+                    element.className = "skillbox skillbox-untrained";
+                    break;
+            }
+        }
+    }
+
     ko.applyBindings(viewModel)
 }
