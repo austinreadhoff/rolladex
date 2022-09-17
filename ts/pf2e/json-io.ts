73753016d871bf551b72dc1abc92615b20eb3e2e
austinread <aread1800@gmail.com>
2022-09-16 20:44:11 -0400

    Saving and loading

---
+++ ts/pf2e/json-io.ts
@@ -0,0 +1,37 @@
+import * as ko from "knockout";
+const koMapping = require('knockout-mapping');
+import { ipcRenderer } from "electron";
+import { switchTab } from "./pf2e";
+import { jsonSchemaVersion, UpgradeSchema } from "./character-schema";
+import { viewModel } from "./viewmodel";
+import { resetSafeSave, triggerUnsafeSave } from "../shared/save-tracker";
+
+ipcRenderer.on('request-save-json', (event: any, arg: any) => {
+    viewModel.character().version(jsonSchemaVersion);
+    var json: any = JSON.parse(ko.toJSON(viewModel.character()));   //the extra .parse ensures functions are excluded
+
+    document.title = viewModel.character().characterName() + " - RollaDex";
+    ipcRenderer.send('send-save-json', json);
+});
+
+ipcRenderer.on('send-loaded-json', (event: any, json: any) => {
+    if (json["version"] < jsonSchemaVersion){
+        json = UpgradeSchema(json);
+    }
+
+    koMapping.fromJS(json, {}, viewModel.character);
+
+    //fromJS will not run constructors for loaded children, and therefore save trackers will not be initiated
+    //the following is a janky workaround, sorry
+    document.querySelectorAll('input').forEach(input => {
+        if (input.classList.contains("attack-stat")){
+            input.addEventListener('input', event => {
+                triggerUnsafeSave(viewModel.character().characterName());
+            });
+        }
+    });
+
+    switchTab("stats");
+
+    resetSafeSave(viewModel.character().characterName());
+});
